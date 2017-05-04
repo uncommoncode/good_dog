a79caec60f5ffa714bd2465edcfb3cfffd3874b3
Sasha Sheng <sash@fb.com>
2017-05-04 12:41:38 -0700

    modified to make it take more than 100 images

--- scrape_google_images_1k.py
+++ scrape_google_images_1k.py
@@ -1,72 +1,73 @@
-from bs4 import BeautifulSoup
+from good_dog.data import require_directory
 from PIL import Image
+from selenium import webdriver
 import urllib.request 
+import urllib.parse
 import requests
 import re
 import os
 import http.cookiejar
 import json
 import base64
-
-def get_soup(url,header):
-    return BeautifulSoup(urllib.request.urlopen(urllib.request.Request(url,headers=header)),'html.parser')
+import time
 
 query = input()
 image_type="ActiOn"
 query= query.split()
 query='+'.join(query)
 print(query)
-url="https://www.google.com/search?q="+query+"&source=lnms&tbm=isch"
-print(url)
+
 #add the directory for your image here
 DIR="Pictures"
-header={'User-Agent':"Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/43.0.2357.134 Safari/537.36"
-}
-soup = get_soup(url,header)
+driver = webdriver.Firefox()
 ActualImages=[]
 
-def get_images(start):
-    screen_width = 1920
-    screen_height = 1080
-    params = {
-        "q": query,
-        "sa": "X",
-        "biw": screen_width,
-        "bih": screen_height,
-        "tbm": "isch",
-        "ijn": start/100,
-        "start": start,
-        #"ei": "" - This seems like a unique ID, you might want to use it to avoid getting banned. But you probably still are.
-    }
-    for a in soup.find_all("div",{"class":"rg_meta"}):
-        link, Type =str(json.loads(a.text)["ou"]),str(json.loads(a.text)["ity"])
-        ActualImages.append((link,Type))
+url="https://www.google.com/search?q="+query+"&source=lnms&tbm=isch&safe=active"
+print("attempting to get url:" + url)
+driver.get(url)
+number_of_scrolls = 100
+for _ in range(number_of_scrolls):
+    for __ in range(10):
+        # multiple scrolls needed to show more than 100 images
+        driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+        time.sleep(0.2)
+    time.sleep(0.5)
+    try:
+        driver.find_element_by_xpath("//input[@value='Show more results']").click()
+    except Exception as e:
+        print("Can't find button for more results:", e)
+        break
 
-for x in range(0, 10):
-    get_images(x*100)
+imgs = driver.find_elements_by_xpath("//div[@class='rg_meta']")
+img_count = 0
+for img in imgs:
+    img_count = img_count+1
+    img_url = json.loads(img.get_attribute('innerHTML'))["ou"]
+    img_type = json.loads(img.get_attribute('innerHTML'))["ity"]
+    print("Image", img_count, ": ", img_url)
+    ActualImages.append((img_url, img_type))
 
 print("there are total", len(ActualImages),"images")
 
-if not os.path.exists(DIR):
-            os.mkdir(DIR)
+require_directory(DIR)
 DIR = os.path.join(DIR, query.split()[0])
-
-if not os.path.exists(DIR):
-            os.mkdir(DIR)
+require_directory(DIR)
 ###print images
 for i , (img , Type) in enumerate(ActualImages):
     try:
+        header={'User-Agent':"Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/43.0.2357.134 Safari/537.36"}
         req = urllib.request.Request(img, headers=header)
         raw_img = urllib.request.urlopen(req)
         raw_img_bytes = raw_img.read()
-        print("img saved:", img)
+        print("img downloaded", i, img)
 
         cntr = len([i for i in os.listdir(DIR) if image_type in i]) + 1
         if len(Type)==0:
-            f = open(os.path.join(DIR , image_type + "_"+ str(cntr)+".jpg"), 'wb')
+            suffix = ".jpg"
         else :
-            f = open(os.path.join(DIR , image_type + "_"+ str(cntr)+"."+Type), 'wb')
+            suffix = "." + Type
 
+        f = open(os.path.join(DIR , image_type + "_"+ str(cntr)+".jpg"), 'wb')
         f.write(raw_img_bytes)
         f.close()
     except Exception as e:
