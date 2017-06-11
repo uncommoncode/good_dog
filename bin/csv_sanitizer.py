48bb1eb89720abeae37c9f51e53061321351ed56
Sasha Sheng <sash@fb.com>
2017-06-10 22:55:01 -0700

    created csv files for faster input read

---
+++ bin/csv_sanitizer.py
@@ -0,0 +1,25 @@
+# Goal: This script is used to read from the train/test directory to generate the csv file so that notebook can read it quickly
+import os
+import csv
+
+def consolidate_file_from_directory(directory, o_dir):
+  with open(o_dir, 'w') as csvfile:
+    writer = csv.writer(csvfile, delimiter=',')
+    for file_name in os.listdir(directory):
+      row = []
+      if file_name.endswith(".jpg"):
+        file_path = os.path.join(directory, file_name)
+        file_name_wo_extension = os.path.splitext(file_name)[0]
+        file_name_deconstructed = file_name_wo_extension.split('_')
+        if len(file_name_deconstructed) < 3:
+          continue
+        map_name = file_name_deconstructed[0] + "_" + file_name_deconstructed[1] + ".mask.0_new.jpg"
+        map_file_path = os.path.join(directory, map_name)
+        if os.path.exists(map_file_path):
+          row.append(file_path)
+          row.append(map_file_path)
+      writer.writerow(row)
+
+consolidate_file_from_directory(os.path.join(os.path.dirname(os.getcwd()), "Pictures", "train"), os.path.join(os.path.dirname(os.getcwd()), "Pictures", "train.csv"))
+consolidate_file_from_directory(os.path.join(os.path.dirname(os.getcwd()), "Pictures", "test"), os.path.join(os.path.dirname(os.getcwd()), "Pictures","test.csv"))
+
